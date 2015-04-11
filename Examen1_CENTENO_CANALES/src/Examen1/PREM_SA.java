a79b89bc117b1d474953b0c12262e1f65f95ee9e
Berandal <usogeneral3@gmail.com>
2015-04-11 00:09:40 -0600

    Se hace un comentario en la clase PREM_SA
    Se realiza un comentario para poder ver que Git reporta el cambio y asi
    poder sincronizar con el repositorio de git hub

--- Examen1_CENTENO_CANALES/src/Examen1/PREM_SA.java
+++ Examen1_CENTENO_CANALES/src/Examen1/PREM_SA.java
@@ -3,7 +3,11 @@ package Examen1;
 
 
 public class PREM_SA {
+/*ESTO ES SOLO UNA PRUEBA PARA VERICAR LOS CAMBIOS EN 
+EL REPOSITORIO DE GITHUB, PRIMERO GIT DEBE DE DETECTAR LOS 
+CAMBIOS EN LA CARPETA DE TRABAJO Y LUEGO INFORMAR DEL CAMBIO.
 
+*/
 public static void main(String[] args) {
 Ventana_general inicio= new Ventana_general();
 inicio.setVisible(true);
